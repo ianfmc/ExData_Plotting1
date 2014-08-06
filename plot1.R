b476b4a53e073490047c48174bae21de7fcd331b
ian <ianfmc@gmail.com>
2014-08-05 21:16:14 -0700

    baseline working version

--- plot1.R
+++ plot1.R
@@ -1,37 +1,36 @@
 plot1 <- function () {
   
   print(Sys.time(), format="%H:%M:%S")
-  consumption_data = data.frame(HDate=character(),
-                                Time=character(),
-                                Global_active_power=numeric(),
-                                Global_reactive_power=numeric(),
-                                Voltage=numeric(),
-                                Global_intensity=numeric(),
-                                Sub_metering_1=numeric(),
-                                Sub_metering_2=numeric(),
-                                Sub_metering_3=numeric())
+  consumption_data <- data.frame(Date=character(),
+                                 Time=character(),
+                                 Global_active_power=numeric(),
+                                 Global_reactive_power=numeric(),
+                                 Voltage=numeric(),
+                                 Global_intensity=numeric(),
+                                 Sub_metering_1=numeric(),
+                                 Sub_metering_2=numeric(),
+                                 Sub_metering_3=numeric())
   
-  consumption_data = read.table("household_power_consumption.txt",
+  consumption_data <- read.table("household_power_consumption.txt",
                                 header=TRUE,
-          
+                                ## nrows=700000,
                                 sep=";",
-                                na.string="?")
+                                na.string="?",
+                                stringsAsFactor=FALSE)
 
   ## convert HDate and Time to R Date and Time objects
   
-  consumption_data[["HDate"]] <- as.Date(consumption_data[["HDate"]], format="%d/%m/%Y")
-  consumption_data[["Time"]] <- paste(consumption_data[["HDate"]],
-                                      consumption_data[["Time"]],
-                                      sep=" ")
-  consumption_data[["Time"]] <- strptime(consumption_data[["Time"]], 
-                                    format="%Y-%m-%d %H:%M:%S")
+  consumption_data[,1] <- as.Date(consumption_data[,1], format="%d/%m/%Y")
+  consumption_data[,2] <- paste(consumption_data[,1],
+                                consumption_data[,2],
+                                sep=" ")
+  consumption_data[,2] <- as.POSIXct(consumption_data[,2],
+                                     format="%Y-%m-%d %H:%M:%S")
   
   print(Sys.time())
   
-  start_date <- strptime("2007-02-01 00:00", 
-                         format="%Y-%m-%d %H:%M")
-  end_date <- strptime("2007-02-03 00:00", 
-                       format="%Y-%m-%d %H:%M")
+  start_date <- strptime("2007-02-01 00:00:01", format="%Y-%m-%d %H:%M:%S")
+  end_date <- strptime("2007-02-02 23:59:59", format="%Y-%m-%d %H:%M:%S")
   
   selected_consumption_data <- consumption_data[consumption_data$Time >= start_date,]
   selected_consumption_data <- selected_consumption_data[selected_consumption_data$Time <= end_date,]
@@ -39,13 +38,14 @@ plot1 <- function () {
   gp <- selected_consumption_data$Global_active_power
   
   png("plot1.png", width=480, height=480)
-  hist(gp,
+  hist(gp, 
        main="Global Active Power",
        col="red",
        ylim=c(0,1200),
        ylab="Frequency",
        xlab="Global Active Power (kilowatts)")
   dev.off()
+  
   ## return (consumption_data)  
   print(Sys.time())
 }
